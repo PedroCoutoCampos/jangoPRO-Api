42c4c18027ef818c7fc941d66cee4b1e59de40e9
PedroCoutoCampos <pedrocoutocampos@outlook.com>
2023-04-28 19:11:41 -0300

    :zap: Criando Autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,14 @@
-import { Router, Request, Response } from "express";
-import { CreateUserController } from "./controllers/user/CreateUserController";
+import { Router, Request, Response } from 'express'
+
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
-
-// router.get('/teste', (req: Request, res: Response) => {
-//   return res.json({ ok: true })
-// })
-
 // --- ROTAS USER ---
-router.post("/users", new CreateUserController().handle);
+router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+router.get('/me', new DetailUserController().handle)
 
 export { router };
