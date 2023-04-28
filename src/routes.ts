7b2c016c5bd67970e74b9dda62a8058a46c3133d
PedroCoutoCampos <pedrocoutocampos@outlook.com>
2023-04-28 17:15:09 -0300

    :art: Atualizando

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,9 @@
-import { Router, Response, Request } from 'express'
-
+import { Router, Request, Response } from 'express'
 
 const router = Router();
 
-
-
-router.get('/teste', (req: Request, res: Response) => {  //3333/teste
-    return res.json({ok: true})
+router.get('/teste', (req: Request, res: Response) => {
+  return res.json({ ok: true })
 })
 
-export { router }
+export { router };
