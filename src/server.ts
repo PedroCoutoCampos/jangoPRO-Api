02dd01f237e90617852cc88678469c548ce687b4
Pedro Couto Campos <pedrocoutocampos@outlook.com>
2023-11-21 15:40:08 -0300

    :rocket: Deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors';
-import { router } from './routes'
+import { router } from './pages'
 
 const app = express();
 
